e89cbea2615c68f7f3cc1e3f5ee1f700046e8d1c
lanmingyi <1972399206@qq.com>
2022-06-13 08:05:29 +0800

    cv-vue

---
+++ src/components/TopVUI/k-portal-design/build/buildBlocks.vue
@@ -0,0 +1,93 @@
+<!--
+ * @Description: 页面预览时使用递归组件调用自己，生成布局结构及组件
+ * @Author: 黄婷
+ * @Date: 2021-05-20 18:13:04
+ * @LastEditTime: 2021-07-21 15:20:31
+ * @LastEditors: 黄婷
+ * @FilePath: \vite-demoe:\wwwroot\Vue\MisBoot\misboot-cloud-vue\src\components\TopVUI\k-portal-design\build\buildBlocks.vue
+-->
+<template>
+	<!-- 标签页布局 -->
+	<a-tabs
+		v-if="record.type === 'tabs'"
+		class="grid-row"
+		:default-active-key="0"
+		:tabBarGutter="record.options.tabBarGutter"
+		:type="record.options.type"
+		:size="record.options.size"
+		:tabPosition="record.options.tabPosition"
+		:animated="record.options.animated"
+	>
+		<a-tab-pane v-for="(tabItem, index) in record.columns" :key="index" :tab="tabItem.label">
+			<buildBlocks
+				ref="nestedComponents"
+				@handleReset="$emit('handleReset')"
+				@change="handleChange"
+				v-for="item in tabItem.list"
+				:key="item.key"
+				:record="item"
+			/>
+		</a-tab-pane>
+	</a-tabs>
+	<a-row v-else-if="record.type === 'grid'" :gutter="[record.options.gutters, record.options.vgutters]">
+		<a-col class="grid-col" v-for="(colItem, idnex) in record.columns" :key="idnex" :span="colItem.span || 0">
+			<buildBlocks
+				ref="nestedComponents"
+				@handleReset="$emit('handleReset')"
+				@change="handleChange"
+				v-for="item in colItem.list"
+				:key="item.key"
+				:record="item"
+			/>
+		</a-col>
+	</a-row>
+	<div v-else>
+		<klistItem
+			ref="nestedComponents"
+			@handleReset="$emit('handleReset')"
+			@change="handleChange"
+			:key="record.key"
+			:record="record"
+		>
+		</klistItem>
+	</div>
+</template>
+<script>
+import klistItem from '../component/listItem';
+export default {
+	name: 'buildBlocks',
+	props: {
+		record: {
+			type: Object,
+			required: true,
+		},
+	},
+	components: {
+		klistItem,
+	},
+	mounted() {
+		// console.log(this.record,'view====')
+	},
+	methods: {
+		validationSubform() {
+			// 验证动态表格
+			let nestedComponents = this.$refs.nestedComponents;
+			if (typeof nestedComponents === 'object' && nestedComponents instanceof Array) {
+				for (let i = 0; nestedComponents.length > i; i++) {
+					if (!nestedComponents[i].validationSubform()) {
+						return false;
+					}
+				}
+				return true;
+			} else if (typeof nestedComponents !== 'undefined') {
+				return nestedComponents.validationSubform();
+			} else {
+				return true;
+			}
+		},
+		handleChange(value, key) {
+			this.$emit('change', value, key);
+		},
+	},
+};
+</script>
---
+++ src/components/TopVUI/k-portal-design/build/index.vue
@@ -0,0 +1,35 @@
+<!--
+ * @Description: 将数据buildBlocks通过组件解析，生成控件
+ * @Author: 黄婷
+ * @Date: 2021-05-20 18:13:04
+ * @LastEditTime: 2021-05-21 14:11:52
+ * @LastEditors: 黄婷
+ * @FilePath: \misboot-cloud-topvui\src\components\TopVUI\k-portal-design\build\index.vue
+-->
+<template>
+	<div>
+		<buildBlocks v-for="(record, index) in value.list" :key="index" :record="record" />
+	</div>
+</template>
+<script>
+import buildBlocks from './buildBlocks';
+export default {
+	props: {
+		value: {
+			type: Object,
+			required: true,
+		},
+		selectItem: {
+			type: Object,
+			default: () => {},
+		},
+		hideModel: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	components: {
+		buildBlocks,
+	},
+};
+</script>
